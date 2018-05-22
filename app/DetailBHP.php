738152d6606967092e8df475942188fa85fa34f2
Cecep Saefudin <saefudincecep@gmail.com>
2018-05-22 07:49:39 +0700

    fix detail bhp

--- app/DetailBHP.php
+++ app/DetailBHP.php
@@ -8,7 +8,7 @@ class DetailBHP extends Model
 {
     protected $table='data_detail_pengajuan';
     protected $primaryKey='id_detail_pengajuan';
-    public $incrementing =false;
+    public $incrementing =true;
     public $timestamps=false;
         protected $fillable = [
             'no_pengajuan','id_perawat','bhp','status',
